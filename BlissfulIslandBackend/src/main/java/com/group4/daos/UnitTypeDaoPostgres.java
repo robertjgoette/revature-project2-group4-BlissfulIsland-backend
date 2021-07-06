cbb478e2553c04acee90bfef486a7770432d0caf
robertjgoette <54007387+robertjgoette@users.noreply.github.com>
2021-07-05 22:45:57 -0500

    Update UnitTypeDaoPostgres.java

--- BlissfulIslandBackend/src/main/java/com/group4/daos/UnitTypeDaoPostgres.java
+++ BlissfulIslandBackend/src/main/java/com/group4/daos/UnitTypeDaoPostgres.java
@@ -68,14 +68,21 @@ public class UnitTypeDaoPostgres implements UnitTypeDAO{
     public ArrayList<HashMap<String, Object>> getAvailableUnitTypes() {
 
         try(Connection connection = ConnectionUtil.createConnection()){
-            String sql = "select unit_type.type_name, unit_type.type_id, unit.unit_apartment_number,\n" +
-                    " unit_type.type_beds, unit_type.type_baths, unit_type.type_cost,count(unit.type_id ) as available,\n" +
-                    "(select count(unit.type_id) from unit where unit.type_id = unit_type.type_id) as total_unit\n" +
-                    "from unit_type \n" +
-                    "left join unit \n" +
-                    "on unit.type_id = unit_type.type_id and unit.unit_is_available =true \n" +
-                    "group by unit_type.type_name,unit_type.type_beds, unit_type.type_baths,\n" +
-                    "unit_type.type_cost, unit_type.type_id , unit.unit_apartment_number ";
+//            String sql = "select unit_type.type_name, unit_type.type_id, unit.unit_apartment_number,\n" +
+//                    " unit_type.type_beds, unit_type.type_baths, unit_type.type_cost,count(unit.type_id ) as available,\n" +
+//                    "(select count(unit.type_id) from unit where unit.type_id = unit_type.type_id) as total_unit\n" +
+//                    "from unit_type \n" +
+//                    "left join unit \n" +
+//                    "on unit.type_id = unit_type.type_id and unit.unit_is_available =true \n" +
+//                    "group by unit_type.type_name,unit_type.type_beds, unit_type.type_baths,\n" +
+//                    "unit_type.type_cost, unit_type.type_id , unit.unit_apartment_number ";
+
+            String sql ="select unit_type.type_name, unit_type.type_id, count(unit.type_id) as available,\n" +
+            "(select count(unit.type_id) from unit where unit.type_id = unit_type.type_id) as total_unit\n" +
+            "from unit_type \n" +
+            "left join unit \n" +
+            "on unit.type_id = unit_type.type_id and unit.unit_is_available =true \n" +
+            "group by unit_type.type_name, unit_type.type_id";
             PreparedStatement ps = connection.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
 
@@ -86,10 +93,6 @@ public class UnitTypeDaoPostgres implements UnitTypeDAO{
 
                 availableUnits.put("typeName",rs.getString("type_name"));
                 availableUnits.put("typeId", rs.getInt("type_id"));
-                availableUnits.put("aptNumber", rs.getInt("unit_apartment_number"));
-                availableUnits.put("typeBeds", rs.getInt("type_beds"));
-                availableUnits.put("typeBaths", rs.getFloat("type_baths"));
-                availableUnits.put("typeCost", rs.getFloat("type_cost"));
                 availableUnits.put("available", rs.getInt("available"));
                 availableUnits.put("totalUnitPerType", rs.getInt("total_unit"));
 
